3300fa07cc9c0cbb578a740f3b25c62afc684157
elavarasanByjus <96741601+elavarasantnl@users.noreply.github.com>
2022-02-16 12:46:53 +0530

    added test case

---
+++ TestBitrise/TestBitriseTests/TestViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TestViewController.swift
+//  TestBitriseTests
+//
+//  Created by Elavarasan on 16/02/22.
+//
+
+import XCTest
+
+@testable import TestBitrise
+
+class TestViewController: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testViewDidLoad(){
+        let st = UIStoryboard(name: "Main", bundle: nil)
+        
+        let myViewController = st.instantiateViewController(withIdentifier: "ViewController") as! ViewController
+        _ = myViewController.view
+        XCTAssertTrue(myViewController.label.text == "Hello World")
+
+    }
+
+    
+}
